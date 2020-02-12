0d2a4a1866ee424bbaeec3a9cee2920649e74aa8
queeniekwan <60158228+queeniekwan@users.noreply.github.com>
2020-02-12 13:35:05 -0500

    Update ex_07.py

--- session07/ex_07.py
+++ session07/ex_07.py
@@ -4,20 +4,20 @@ from tabulate import tabulate
 def mysqrt(a):
     x = a / 2
     while True:
-        y = (x + a / x) / 2
+        y = (x + a/x) / 2
         if x == y:
             break
         x = y
     return x
 
-def test_square_root():
-    results = [(a, mysqrt(a), sqrt(a), sqrt(a) - mysqrt(a)) for a in range(1,10)]
+def test_square_root(min, max):
+    results = [(a, mysqrt(a), sqrt(a), abs(sqrt(a) - mysqrt(a))) for a in range(min,max)]
     print(tabulate(results, headers=['a', 'mysqrt(a)', 'math.sqrt(a)', 'diff'], numalign='left'))
 
 
 def main():
     # print(mysqrt(9))
-    test_square_root()
+    test_square_root(1, 10)
 
 if __name__ == "__main__":
     main()
