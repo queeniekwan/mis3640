394a835f2bb1bd9ea53820fa09b946d7e506c4af
Queenie Kwan <60158228+queeniekwan@users.noreply.github.com>
2020-03-03 17:11:17 -0500

    session12

---
+++ session12/tuple_demo.py
@@ -0,0 +1,53 @@
+# Tuples are immutable
+# to create a tuple variable with only one element, close the statement with a ','
+
+t = 'a',
+print(t)
+
+a = tuple('Babson')
+print(a)
+
+b = tuple([1, 2, 3])
+print(b)
+
+# to modify tuple
+b = (0, ) + b[:]
+print(b)
+
+# using tuple to swap values of two variables
+x = 10
+y = 90
+x, y = y, x 
+print(x, y)
+
+# using tuple to split a string and store elements in variables
+email = 'yguan3@babson.edu'
+id, domain = email.split('@')
+print(f'id: {id}; domain: {domain}')
+
+tel = '508-333-0452'
+area, _, local = tel.split('-')
+print(area)
+print(local)
+
+# Tuples as reurn values
+t = divmod(7, 3) 
+print(t)
+
+def f():
+    return 1, 2
+
+type(f()) # is a tuple
+a, b = f()
+print(a)
+print(b)
+
+# Variable-length argument tuples
+def sumall(*nums):
+    result = 0
+    for num in nums:
+        result += num
+    return result
+
+print(sumall(1))
+print(sumall(1, 2, 3))
