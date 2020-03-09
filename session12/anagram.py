6f4d2859f3ba56c536e9bf3480ed2b675efb75cc
queeniekwan <60158228+queeniekwan@users.noreply.github.com>
2020-03-08 22:47:37 -0400

    anagram exercise

--- session12/anagram.py
+++ session12/anagram.py
@@ -13,49 +13,37 @@ e.g. {'eemrru':['rumeer', 'reemur'], 'ekinz':['kenzi']}
 
 4. sort the anagram with its length, create a dict(length: a list of lists)
 
-
-
 """
 def read_file_to_list(path):
     '''
     reads all the words from the file
     returns a list of words
     '''
+    f = open(path)
+    word_list = f.read().lower().split()
 
-def signature_set(words):
-    '''
-    the sorted string of each word is its signature
-    returns a list of all unique signature in the list words
-    words is a list
-    '''
-    signature = []
-    for word in words:
-        signature.append(sorted(word))
-    signature = list(set(signature))
+    return word_list
 
-    return signature
-
-def anagram(signatures, words):
+def anagram(words):
     '''
-    signature is a list of signatures
-    words is a list of words
-    returns a dictionary of signatures as keys 
-        and lists of annagram of that signature as values
+    returns a dictionary of anagrams
+    keys is a signature (sorted string) of the anagram words 
+    values is a list of anagram words
     '''
     d = {}
-    for signature in signatures:
-        anagram = []
-        for word in words:
-            if signature == sorted(word):
-                anagram.append(word)
-        d[signature] = anagram
-    
+    for word in words:
+        s = ''.join(sorted(word))
+        d.setdefault(s, []).append(word)
+
     return d
 
 def print_anagrams(word_dict, n = 1):
     '''
     prints all the anagrams with more than n words
     '''
+    for signature, words in word_dict.items():
+        if len(words) > n:
+            print(f'{signature}: {words}')
 
 def sorted_dict(d):
     '''
@@ -63,11 +51,18 @@ def sorted_dict(d):
     key is numbe of words in a anagram list, 
     value is a list of anagram lists
     '''
+    sd = {}
+    for words in d.values():
+        sd.setdefault(len(words), []).append(words)
+
+    return sd
 
 def main():
     word_list = read_file_to_list('session09/words.txt')
-    word_dict = anagram(signature_set(word_list), word_list)
-    print_anagrams(word_dict)
+    word_dict = anagram(word_list)
+    print_anagrams(word_dict, 2)
+    sd = sorted_dict(word_dict)
+    # print_anagrams(sd, 10)
 
 if __name__ == "__main__":
     main()
