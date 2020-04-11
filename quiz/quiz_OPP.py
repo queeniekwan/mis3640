b3a8d1350d130ffeb0980cec1cbc5fd03fb13d99
queeniekwan <60158228+queeniekwan@users.noreply.github.com>
2020-04-11 16:28:18 -0400

    Update quiz_OPP.py

--- quiz/quiz_OPP.py
+++ quiz/quiz_OPP.py
@@ -1,5 +1,7 @@
 class Course():
+    ''' Represents a course in Babson College '''
     def __init__(self, id='', title='', instructors=[], credits=4, enrolled=0, capacity=0):
+        ''' attributes of an object Course: id, title, instructors, credits, enrolled, capacity '''
         self.id = id
         self.title = title
         self.instructors = instructors
@@ -8,23 +10,26 @@ class Course():
         self.capacity = capacity
     
     def __str__(self):
+        ''' prints the course'''
         instructors = ', '.join(self.instructors)
         return f'{self.id}  {self.title}  {instructors}  {self.credits:.2f}  {self.enrolled}/{self.capacity}'
     
     def add_course(self, number_of_students=1):
+        ''' increases the number of enrolled students of a course when new students join'''
         self.enrolled += number_of_students
-        print(f'{number_of_students} student(s) joined {self.title}! Now we have {self.enrolled} students.')
-
+    
     def drop_course(self, number_of_students=1):
+        ''' decreases the number of enrolled students of a course when students drop'''
         self.enrolled -= number_of_students
-        print(f'{number_of_students} student(s) dropped {self.title}! Now we have {self.enrolled} students.')
 
     def change_instructor(self, old_instructor, new_instructor):
+        ''' replaces an old instructor with a new instructor'''
         for i, name in enumerate(self.instructors):
             if name == old_instructor:
                 self.instructors[i] = new_instructor
     
     def __or__(self, other):
+        ''' advises student which course to take based on current enrollment situation'''
         if self.enrolled < self.capacity and other.enrolled < other.capacity:
             return f'You can take both courses! They both have available spots.'
         elif self.enrolled >= self.capacity and other.enrolled >= other.capacity:
@@ -41,14 +46,15 @@ def main():
     print(mac_tom)
 
     # print(mis | mac_tom)
-    mis.drop_course(2)
+    # mis.drop_course(2)
     # print(mis | mac_tom)
-    mac_tom.drop_course()
+    # mac_tom.drop_course()
     # print(mis | mac_tom)
+    # mis.add_course()
+    # print(mis)
 
-    mac_tom.change_instructor('Richard Block', 'Richard Goulding')
-    print(mac_tom)
-
+    # mac_tom.change_instructor('Richard Block', 'Richard Goulding')
+    # print(mac_tom)
 
 if __name__ == "__main__":
     main()
