60c18bb3ef203ac7dba2970bd5668ec29d45b7c1
queeniekwan <60158228+queeniekwan@users.noreply.github.com>
2020-04-14 15:09:10 -0400

    OOP Quiz 2

--- quiz/candidate.py
+++ quiz/candidate.py
@@ -4,27 +4,39 @@ ELECTORS = {'CA': 55, 'TX': 38, 'FL': 29, 'MA': 11}
 class Candidate:
     """The presidential candidate"""
 
-    def __init__(self, name, winning_states=None, votes=0):
+    def __init__(self, name, winning_states=[], votes=0):
         """Initialize candidate.
 
         name: string
         winning_states: a list of strings representing initial winning state(s).
         votes: integer, representing number of votes
         """
-        pass
+        self.name = name
+        self.winning_states = winning_states
+        self.votes = votes
+        for state in winning_states:
+            self.votes += ELECTORS[state]
 
     def __str__(self):
         """Return a string representaion of this candidate,
         including name and winning state(s).
         """
-        pass
+        winning = ' '.join(self.winning_states)
+        return f'{self.name} wins {winning}'
 
     def win_state(self, state):
         """Adds a tate to winning_states and updates votes.
 
         state: a string of state abbreviation
         """
-        pass
+        self.winning_states.append(state)
+        self.votes += ELECTORS[state]
+
+    def __gt__(self, other):
+        """Compares votes of two candidates
+        returns True if self has more votes, False otherwise
+        """
+        return self.votes > other.votes
 
 
 def main():
@@ -41,7 +53,8 @@ def main():
     print(clinton)
     print('Does Trump win?')
     print(trump > clinton)
-
+    print(trump.votes)
+    print(clinton.votes)
 
 if __name__ == '__main__':
     main()
